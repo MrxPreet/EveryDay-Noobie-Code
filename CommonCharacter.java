422f7dd4e9f9ac61cc956ec27b92329836a3b7d9
Preet Patel <114079367+MrxPreet@users.noreply.github.com>
2023-02-06 11:12:19 -0500

    Update CommonCharacter.java

--- CommonCharacter.java
+++ CommonCharacter.java
@@ -1,3 +1,5 @@
+package code;
+
 /******************************************************************************
  * This code implements a solution to find common characters in multiple
  * strings. It is intended for beginners who are looking for a basic
@@ -5,8 +7,27 @@
  * runtime. Advanced users who require faster performance can use more optimized
  * methods available online.
  *******************************************************************************/
- 
-	public static String FindString(String[] str) {
+
+import java.util.Arrays;
+public class CommonCharacters {
+	// Main Method
+	public static void main(String[] args) {
+		String result = findString(new String[] { "Hello-World", "Mrx-Noobie", "Life-Of-Money" });
+		System.out.println("1: "+result);
+		
+		result = findString(new String[] { "result", "public static", "console", "comments"});
+		System.out.println("2: " + result);
+		
+		result = findString(new String[] { "ABCDE", "XYZA", "YCDK", "WWW.HELLOWORLD.COM"});
+		System.out.println("3: " + result);
+		/**************
+		 * Console
+		 * 1: -eo
+		 * 2: s
+		 * 3: NONE!
+		 *************/
+	}
+	public static String findString(String[] str) {
 		String findedString = "";
 		// loop through the string array
 		for (int i = 0; i < str.length; i++) {
@@ -17,11 +38,12 @@
 			// convert the sorted character array back to a string
 			str[i] = new String(charArray); // 3
 		}
+
 		// loop through the first string in the string array
 		for (int i = 0; i < str[0].length(); i++) {
 			int temp = 0;
-			// loop through the next 3 strings in the string array
-			for (int j = 1; j < 4; j++) {
+			// loop through the next strings in the string array
+			for (int j = 1; j < str.length; j++) {
 				// loop through the characters in the current string
 				for (int k = 0; k < str[j].length(); k++) {
 					// check if the current character in str[0] is in str[j]
@@ -37,8 +59,8 @@
 					}
 				}
 			}
-			// if the current character is in all 4 strings, add it to the findedString
-			if (temp == 3) {
+			// if the current character is in all strings, add it to the findedString
+			if (temp == str.length - 1) {
 				findedString = findedString + str[0].charAt(i);
 			}
 		}
@@ -46,6 +68,7 @@
 		if (findedString.equals("")) {
 			findedString = "NONE!";
 		}
-
 		return findedString;
 	}
+
+}
